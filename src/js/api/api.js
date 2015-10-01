506c315b6417dbf98aa95d0050ab9bcaa2a29c56
souhe <souhepend@hotmail.com>
2015-10-01 22:27:42 +0200

    Added JSON parse to API

--- src/js/api/api.js
+++ src/js/api/api.js
@@ -2,6 +2,20 @@ import xhttp from 'xhttp';
 
 class Api {
     static get(url){
+        return new Promise((resolve, reject) => {
+            xhttp({
+                url: url,
+                method: 'get'
+            }).then(result => {
+                if(typeof result === 'string'){
+                    resolve(JSON.parse(result));
+                }else{
+                    resolve(result);
+                }
+            }).catch((error, xhr) => {
+                reject(error, xhr);
+            })
+        });
         return xhttp({
             url: url,
             method: 'get'
